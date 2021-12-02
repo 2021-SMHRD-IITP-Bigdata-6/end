de2d6270fce5eec9c4d6ea0402ebdd32cec9543e
SMT107 <SMT107@DESKTOP-7LGMOCB>
2021-12-02 16:18:08 +0900

    test

--- GitTest/src/Test.java
+++ GitTest/src/Test.java
@@ -3,16 +3,12 @@ public class Test {
 
 	public static void main(String[] args) {
 		//Github 환경설정
-
-		//1. JDK 버전
-		//2. Tomcat 버전
-		//3. Encoding 방식(UTF-8)
-
 		// 1. JDK 버전123456789
 		// 2. Tomcat 버전123456789
 		// 3. Encoding 방식(UTF-8)123456789
 		System.out.println("First Code!");
 
+		
 	}
 
 }
