9f8befe515bf4022352fe4751df7cbc0348ae2b3
SMT103 <SMT103@DESKTOP-EFS05VJ>
2021-12-02 16:29:48 +0900

    이렇게 바뀌는지 체크하는 코드

--- GitTest/src/Test.java
+++ GitTest/src/Test.java
@@ -6,7 +6,9 @@ public class Test {
 		// 1. JDK 버전123456789
 		// 2. Tomcat 버전123456789
 		// 3. Encoding 방식(UTF-8)123456789
+		System.out.println("이렇게바꾸면 바뀌는걸로 들어가는건가?");
 		System.out.println("First Code!");
+		
 
 	}
 
