9f81c1a9377770118147002c2a2acdd52d233923
SMT108 <SMT108@DESKTOP-P1LCRUQ>
2021-12-02 16:06:04 +0900

    아무거나

--- GitTest/src/Test.java
+++ GitTest/src/Test.java
@@ -2,6 +2,10 @@
 public class Test {
 
 	public static void main(String[] args) {
+		//Github 환경설정
+		//1. JDK 버전
+		//2. Tomcat 버전
+		//3. Encoding 방식(UTF-8)
 		System.out.println("First Code!");
 
 	}
